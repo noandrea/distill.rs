e485598bcc1c3e6e83a78b944c7a2473e36edd1d
Andrea Giacobino <no.andrea@gmail.com>
2020-04-02 12:11:00 +0200

    test: add tests

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,25 @@
-#[macro_use]
 extern crate blake3
 
 // compute the hash of a string 
 fn hash(val: str) -> str {
     let h = blake3::hash(val.as_bytes())
     hex::encode(h)
+}
+
+// generate a short code of a certain length from 
+// a preset list of characters
+fn gen_code(length: u16, alphabet: String) {
+    
+} 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash() -> Result<(), String> {
+        let s, h = ("aaa", "bbbb") 
+        assert_eq!(hash(s), h);
+        Ok(())
+    }
 }
